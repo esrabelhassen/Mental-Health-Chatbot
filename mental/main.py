c2626880edcb64dd2f2fd30d1ab8bd50704d6eb1
BELHASSEN ISRA <139223756+esrabelhassen@users.noreply.github.com>
2026-03-23 21:49:32 +0100

    Update main.py

--- mental/main.py
+++ mental/main.py
@@ -2,11 +2,9 @@ from core.loader import load_scale
 from core.state import init_state, get_next_question, record_response
 from core.scoring import calculate_score, interpret_score
 
-# Load PHQ-9 as example
 scale_data = load_scale("PHQ-9")
 state = init_state("PHQ-9")
 
-# Simulate answering questions
 while True:
     q = get_next_question(scale_data, state)
     if not q:
@@ -16,7 +14,6 @@ while True:
     ans = int(input("Your answer (0-3): "))
     record_response(state, q["id"], ans)
 
-# Calculate score
 score = calculate_score(scale_data, state["responses"])
 interpretation = interpret_score(scale_data, score)
 
